efb9edc08fe4a2cac6f655d890c9f265849d6655
Mohit <mohituniyal1234567@gmail.com>
2026-09-05 23:22:56 +0530

    feat: add default scanner configuration and test-project environment settings

--- test-project/config.js
+++ test-project/config.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ayanozoro/secretGuard.git
+git push -u origin mainrequire('dotenv').config();
 
 module.exports = {
   apiKey: process.env.API_KEY || 'default-key',
